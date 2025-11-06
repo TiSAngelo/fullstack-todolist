ff380120a2cc644229b4c38247ddf067b9992c2e
Tiago Angelo <tiago.angelo@crearesistemas.com.br>
2025-11-06 17:06:41 -0300

    feat: Adiciona builder em TaskDto, Remove @Autowired, aplica retorno de ResponseEntity, utiliza builder para criacao de objetos

--- todo-list-api/src/main/java/estudos/todo_list_api/entity/Task.java
+++ todo-list-api/src/main/java/estudos/todo_list_api/entity/Task.java
@@ -1,10 +1,7 @@
 package estudos.todo_list_api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "todo")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- todo-list-api/src/main/java/estudos/todo_list_api/repository/TaskRepository.java
+++ todo-list-api/src/main/java/estudos/todo_list_api/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package estudos.todo_list_api.repository;
 
 import estudos.todo_list_api.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository <Task, Long> {
+    @Query(value = "SELECT * FROM todo WHERE type_id = :typeId", nativeQuery = true)
     List<Task> findByTypeId(Long typeId);
 }
